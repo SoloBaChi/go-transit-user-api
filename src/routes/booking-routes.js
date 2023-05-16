e1bf4de1f8ca68ccca1c2618714129fa03b28ce0
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-05-16 23:43:51 +0100

    added get route for the books

--- src/routes/booking-routes.js
+++ src/routes/booking-routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const createBooking  = require('../controllers/booking-controllers')
+const { createBooking , getBookedDoc}  = require('../controllers/booking-controllers')
 
 
 bookingRouter = express.Router()
 
 
 bookingRouter.post("/create", createBooking)
+bookingRouter.get("/getbook/:id",getBookedDoc)
 
 module.exports =  bookingRouter;
